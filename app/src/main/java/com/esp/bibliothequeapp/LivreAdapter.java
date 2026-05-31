f4568b539eb0dbcf72b1a21dba51f63194d504b0
CARIONS STUDIOS <carionsstudios@gmail.com>
2026-05-31 11:35:58 +0000

    Lab 4 : Displaying a list of books with RecyclerView

---
+++ app/src/main/java/com/esp/bibliothequeapp/LivreAdapter.java
@@ -0,0 +1,76 @@
+package com.esp.bibliothequeapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.graphics.Color;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class LivreAdapter extends RecyclerView.Adapter<LivreAdapter.LivreViewHolder> {
+    // List of books to display
+    private ArrayList<Livre> listeLivres;
+
+    // Adapter constructor
+    public LivreAdapter(ArrayList<Livre> listeLivres) {
+        this.listeLivres = listeLivres;
+    }
+
+    @NonNull
+    @Override
+    public LivreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Coverts the XML file into an object view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_livre, parent, false);
+        return new LivreViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LivreViewHolder holder, int position) {
+        // Retrieve the book from the current position.
+        Livre livre = listeLivres.get(position);
+
+        // Inject the book data into the views
+        holder.tvTitreLivre.setText(livre.getTitre());
+        holder.tvAuteurLivre.setText("Auteur : " + livre.getAuteur());
+        holder.tvIsbnLivre.setText("ISBN : " + livre.getIsbn());
+
+        // Availability badge management
+        if (livre.isDisponible()) {
+            holder.tvDisponibilite.setText("Disponible");
+            holder.tvDisponibilite.setBackgroundColor(Color.parseColor("#2E7D32"));
+        } else {
+            holder.tvDisponibilite.setText("Indisponible");
+            holder.tvDisponibilite.setBackgroundColor(Color.parseColor("#C62828"));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        // Returns the total number of items to display
+        return listeLivres.size();
+    }
+
+    // Internal static viewholder
+    public static class LivreViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitreLivre;
+        TextView tvAuteurLivre;
+        TextView tvIsbnLivre;
+        TextView tvDisponibilite;
+
+        public LivreViewHolder(@NotNull View itemView) {
+            super(itemView);
+
+            // Linking XML components and Java variables
+            tvTitreLivre = itemView.findViewById(R.id.tvTitreLivre);
+            tvAuteurLivre = itemView.findViewById(R.id.tvAuteurLivre);
+            tvIsbnLivre = itemView.findViewById(R.id.tvIsbnLivre);
+            tvDisponibilite = itemView.findViewById(R.id.tvDisponibilite);
+        }
+    }
+}
---
+++ app/src/main/java/com/esp/bibliothequeapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.esp.bibliothequeapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // List display component
+    private RecyclerView recyclerViewLivres;
+
+    // Custom fit
+    private LivreAdapter livreAdapter;
+
+    // List of books
+    private ArrayList<Livre> listeLivres;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Associates the XML layout to the activity
+        setContentView(R.layout.activity_main);
+
+        // Retrieving the button via its ID
+        /*Button btnVoirLivres = findViewById(R.id.btnVoirLivres);
+
+        // Adding a listener to the click
+        btnVoirLivres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Displays a messages
+                Toast.makeText(MainActivity.this, "Accès à la liste des livres", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+
+        // Retrieving the RecyclerView in the layout
+        recyclerViewLivres = findViewById(R.id.recylerViewLivres);
+
+        // Initializing the list
+        listeLivres = new ArrayList<>();
+
+        // Addition of 8 fictional books
+        listeLivres.add(new Livre(1, "Le Petit Prince", "Antoine de Saint Exupéry", "9780156013987", true));
+        listeLivres.add(new Livre(2, "L'Étranger", "Albert Camus", "9782070360024", false));
+        listeLivres.add(new Livre(3, "Les Misérables", "Victor Hugo", "9782253096344", true));
+        listeLivres.add(new Livre(4, "Une si longue lettre", "Mariama Bâ", "9782841290529", true));
+        listeLivres.add(new Livre(5, "Le Vieux Nègre et la Médaille", "Ferdinand Oyono", "9782264018304", false));
+        listeLivres.add(new Livre(6, "Madame Bovary", "Gustave Flaubert", "9782070409228", true));
+        listeLivres.add(new Livre(7, "La Peste", "Alert Camus", "9782070360420", false));
+        listeLivres.add(new Livre(8, "Sous l'orage", "Seydou Badian", "9782708707691", true));
+
+        // The RecyclerView will display the items vertically
+        recyclerViewLivres.setLayoutManager(new LinearLayoutManager(this));
+
+        // Creating the adapter with the list of books
+        livreAdapter = new LivreAdapter(listeLivres);
+
+        // Link between the RecyclerView and the adapter
+        recyclerViewLivres.setAdapter(livreAdapter);
+    }
+}
